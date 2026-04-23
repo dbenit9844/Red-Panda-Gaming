59ab64128946d6c158615eda933da14d73e8b76c
ajward0202 <ajward02@frostburg.edu>
2026-04-23 19:03:05 -0400

    .

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,33 +2,27 @@ using System.Collections;
 using System.Collections.Generic; 
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
-
     [Header("Player Settings")]
     [SerializeField] float speed;
     [SerializeField] float jumpingPower;
-
     [Header("Grounding")]
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
 
     private float horizontal;
-
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
-
     #region PLAYER_CONTROLS
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
     }
-
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -36,11 +30,9 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
     }
-
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
     #endregion
-
 }
