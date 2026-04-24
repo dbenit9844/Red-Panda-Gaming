be2a272e83d152eac3d91ab3815737f3ad1f0638
ajward0202 <ajward02@frostburg.edu>
2026-04-24 14:49:05 -0400

    made the map ground and fixed the infinite jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,38 +1,59 @@
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
+
     [Header("Player Settings")]
-    [SerializeField] float speed;
-    [SerializeField] float jumpingPower;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpingPower = 10f;
+
     [Header("Grounding")]
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
 
     private float horizontal;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
+
     #region PLAYER_CONTROLS
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
     }
+
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
     }
+    #endregion
+
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapBox(groundCheck.position, new Vector2(0.3f, 0.1f), 0f, groundLayer);
     }
-    #endregion
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(groundCheck.position, new Vector3(0.3f, 0.1f, 0f));
+        }
+    }
 }
