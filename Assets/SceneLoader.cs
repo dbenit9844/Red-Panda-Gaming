619ad188d251cd29be8db2da034e88085e401107
dbenit9844 <dbenit9844@gmail.com>
2026-05-05 22:53:48 -0400

    Fix frozen bug

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -5,11 +5,13 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadMapSelector()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Map Selector");
     }
 
     public void LoadMainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu"); // <-- FIXED (space added)
     }
 }
